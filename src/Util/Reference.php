6ee8d169f99f088a6e0d3ca118baf5456b950634
PauloVitorDB <pvitor.db@gmail.com>
2023-03-31 10:33:01 -0300

    Referências de doc.

--- src/Util/Reference.php
+++ src/Util/Reference.php
@@ -11,15 +11,15 @@ abstract class Reference {
                 $link = "https://dev.pagseguro.uol.com.br/reference/order-pay-credit-encrypted";
 			break;
 			case "boleto":
-                $name = "Criando e pagando um pedido com Boleto";
+                $name = "Criando_e_pagando_um_pedido_com_Boleto";
                 $link = "https://dev.pagseguro.uol.com.br/reference/pay-order-boleto";
 			break;
 			case "debit":
-                $name = "Criar_um_pedido_com_autenticação_3DS_interna";
+                $name = "Criar_um_pedido_com_autenticacao_3DS_interna";
                 $link = "https://dev.pagseguro.uol.com.br/reference/create-order-with-3ds-internal";
 			break;
 			case "public_key":
-                $name = "Criar_suas_chaves_públicas";
+                $name = "Criar_suas_chaves_publicas";
                 $link = "https://dev.pagseguro.uol.com.br/reference/post-public-keys";
 			break;
 			// case "installment":
@@ -27,7 +27,7 @@ abstract class Reference {
             //     $link = "https://dev.pagseguro.uol.com.br/reference/get-transaction-fees";
 			// break;
 			case "session":
-                $name = "Criar_uma_sessão";
+                $name = "Criar_uma_sessao";
                 $link = "https://dev.pagseguro.uol.com.br/reference/post-sessions";
 			break;
 		}
